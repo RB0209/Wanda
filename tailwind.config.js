c78623b73e53b5f9ff4e6ef0c972e39a8259c01d
Rishikesh <kesm3052021@gmail.com>
2025-07-25 04:13:40 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-// tailwind.config.js content
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#38b6ff"
+      }
+    }
+  },
+  plugins: []
+}
